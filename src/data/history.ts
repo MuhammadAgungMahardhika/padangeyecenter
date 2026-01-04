17e2d643026136a16ae6cf9b3f380a3b501d5ab7
agung <m.agungmahardika12@gmail.com>
2026-01-04 18:52:48 +0700

    about

---
+++ src/data/history.ts
@@ -0,0 +1,32 @@
+export const padangEyeCenterTimeline = [
+  {
+    year: "2007",
+    title: "Pendirian PT. Muda Medika Mandiri",
+    description:
+      "PT. Muda Medika Mandiri didirikan berdasarkan Akta No. 08 tanggal 4 Agustus 2007 di Padang dan disahkan oleh Kementerian Hukum dan HAM RI.",
+  },
+  {
+    year: "2008",
+    title: "Berdiri sebagai Klinik Mata",
+    description:
+      "Padang Eye Center mulai beroperasi melayani kesehatan mata masyarakat dengan izin Klinik Mata.",
+  },
+  {
+    year: "2010",
+    title: "Menjadi Rumah Sakit Mata",
+    description:
+      "Pada 6 September 2010, Padang Eye Center resmi beroperasi sebagai Rumah Sakit Mata dengan nama RSKM Padang Eye Center.",
+  },
+  {
+    year: "2025",
+    title: "Pindah Lokasi",
+    description:
+      "Pada 1 Maret 2025, Padang Eye Center berpindah ke tempat baru dengan fasilitas dan gedung yg lebih luas dengan nama RS Mata Pec Khatib.",
+  },
+  {
+    year: "Sekarang",
+    title: "Pelayanan Berbasis Mutu & Hati",
+    description:
+      "Padang Eye Center terus meningkatkan mutu, daya saing, dan pelayanan dengan nilai dasar “Melayani Dengan Hati”.",
+  },
+];
